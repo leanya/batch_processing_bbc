c4eec6d011f53d9a57f9901af096a4643b66d791
LeanYinAi <yin_ai_lean@aiap.sg>
2024-08-07 23:35:34 +0800

    update documentation and backend data preparation of tokens

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# batch_processing_bbc
+# Batch Processing News Headline Text
+
+The project consists of two components - data pipeline and deployment pipeline.  
+The data pipeline involves extracting the dataset from a news website and preparing them for data visualisation using Streamlit. It uses simple cron jobs to schedule daily execution, and is managed through a Docker Compose file.   
+The deployment pipeline focuses on pushing the Docker images to Docker hub and running the Docker Compose file on the AWS EC2 platform.  
+
+#### Data Pipeline
+1. Data Extraction and Data Cleaning
+- Data Scraping of news headline text from BBC website 
+- Data Transformation 
+- Write to Postgresql
+2. Cron jobs to schedule task 1 daily
+3. Data visualisation to view the the most common keywords through wordcloud and the corresponding news headline text
+- Reference the image of the streamlit app 
+
+<img src = "./images/data_pipeline.png" width="50%" height="50%">
+
+#### CI/CD Pipeline
+
+<img src = "./images/cicd_pipeline.png" width="50%" height="50%">
+
+
+#### Snippet of the streamlit app
+<img src = "./images/streamlit.png" width="50%" height="50%">
--- backend/data_prep_dag.py
+++ backend/data_prep_dag.py
@@ -1,14 +1,11 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import nltk
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
 from sqlalchemy import create_engine
 import sqlalchemy
-
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 
 
 def scrape_headline_dataset(url):
@@ -28,34 +25,31 @@ def scrape_headline_dataset(url):
 
 def data_cleaning(df):
 
-    # remove duplicates
+    # Remove duplicates
     df_clean = df.copy()
     df_clean = df_clean.drop_duplicates()
 
-    # drop non-headline datasets
+    # Drop non-headline datasets
     df_clean["count_words"] = df_clean["headline"].str.split().str.len()
     df_clean = df_clean[df_clean["count_words"]> 3]
     df_clean = df_clean.drop(columns = "count_words")
 
     # Data Cleaning to extract common keywords
 
-    # picks out sequences of alphanumeric characters as tokens
-    tokenizer = RegexpTokenizer(r'\w+')
-    df_clean["tokens"] = df_clean["headline"].str.lower()
-    df_clean["tokens"] = df_clean["tokens"].apply(tokenizer.tokenize)
+    # Download nltk packages 
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    
+    # Word Tokenize
+    df_clean["tokens"] = df_clean["headline"].apply(word_tokenize)
 
-    # remove stop words
-    nltk.download('stopwords')
-    stop = stopwords.words('english')
-    df_clean["tokens"] = df_clean["tokens"].apply(lambda x: [word for word in x if word not in (stop)])
-
-    # stemming (lemmatize seems to convert us/'US' to u )
-    nltk.download('wordnet')
-    wordnet = WordNetLemmatizer()
-    df_clean["tokens"] = df_clean["tokens"].apply(lambda x: [wordnet.lemmatize(word) for word in x ])
-    # ps = PorterStemmer()
-    # df_clean["tokens"] = df_clean["tokens"].apply(lambda x: [ps.stem(word) for word in x ])
-
+    # Extract Nouns and Verbs
+    df_clean["tokens"] = df_clean["tokens"].apply(lambda x: 
+                                                  [word for (word, pos) in pos_tag(x) if pos in 
+                                                   ["NN", "NNS", "NNP", "NNPS", 
+                                                    'VB', 'VBG', 'VBD', 'VBN']])
+    
+    # Add etl date information
     df_clean["etl_date"] = pd.to_datetime('today')
     df_clean["etl_date"] = df_clean["etl_date"].dt.normalize()
 
@@ -63,19 +57,22 @@ def data_cleaning(df):
 
 def write_postgres(df):
 
+    # Specify the data types of the columns 
     dtypes = {
         'headline' : sqlalchemy.types.TEXT(),
         'tokens' : sqlalchemy.types.ARRAY(sqlalchemy.types.TEXT()),
         'etl_date' : sqlalchemy.types.DateTime() 
     }
 
+    # Connect and write to postgresql docker 
     engine = create_engine('postgresql+psycopg2://postgres:postgres@db_postgres:5432/postgres')
     df.to_sql(name = "bbc", 
               con = engine, 
               index = False,
               dtype = dtypes,
               if_exists = "append")
-
+    
+    # Close the connection 
     engine.dispose()
 
 def main():
